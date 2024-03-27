333260428642372843d7946694fa86b3b2d34e59
FREDSON SILVA <silva.fredson@hotmail.com>
2024-03-26 21:25:00 -0300

    Initial Commit

---
+++ Swift-API-Calls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-API-Calls
+//
+//  Created by Fredson Silva on 26/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-API-Calls/Swift_API_CallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_API_CallsApp.swift
+//  Swift-API-Calls
+//
+//  Created by Fredson Silva on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_API_CallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
